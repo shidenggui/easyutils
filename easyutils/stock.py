81eba5b8a761325347431902448da4ff02ec8bd3
shidenggui <longlyshidenggui@gmail.com>
2016-04-16 10:25:03 +0800

    init

---
+++ easyutils/stock.py
@@ -0,0 +1,33 @@
+import requests
+
+
+def get_stock_type(stock_code):
+    """判断股票ID对应的证券市场
+    匹配规则
+    ['50', '51', '60', '90', '110'] 为 sh
+    ['00', '13', '18', '15', '16', '18', '20', '30', '39', '115'] 为 sz
+    ['5', '6', '9'] 开头的为 sh， 其余为 sz
+    :param stock_code:股票ID, 若以 'sz', 'sh' 开头直接返回对应类型，否则使用内置规则判断
+    :return 'sh' or 'sz'"""
+    assert type(stock_code) is str, 'stock code need str type'
+    if stock_code.startswith(('sh', 'sz')):
+        return stock_code[:2]
+    if stock_code.startswith(('50', '51', '60', '90', '110', '113', '132', '204')):
+        return 'sh'
+    if stock_code.startswith(('00', '13', '18', '15', '16', '18', '20', '30', '39', '115', '1318')):
+        return 'sz'
+    log.warn(
+            'cant auto decide {code} stock type, use default simple rule, please manually use sz{code}/sh{code}'.format(
+                    code=stock_code))
+    if stock_code.startswith(('5', '6', '9')):
+        return 'sh'
+    return 'sz'
+
+
+def get_all_stock_codes():
+    """获取所有股票 ID 到 all_stock_code 目录下"""
+    all_stock_codes_url = 'http://www.shdjt.com/js/lib/astock.js'
+    grep_stock_codes = re.compile('~(\d+)`')
+    response = requests.get(all_stock_codes_url)
+    stock_codes = grep_stock_codes.findall(response.text)
+    return stock_codes
