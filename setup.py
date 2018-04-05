f8f229bc220adf31981b5fc9bc5482c4bc5554d0
shidenggui <903618848@qq.com>
2018-04-05 16:44:40 +0800

    :star: use bumpversion

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 # coding:utf8
 from setuptools import setup, find_packages
 
-import easyutils
-
 setup(
-        name='easyutils',
-        version=easyutils.__version__,
-        packages=find_packages(),
-        description='A utility for China Stock',
-        author='shidenggui',
-        author_email='longlyshidenggui@gmail.com',
-        license='BSD',
-        url='https://github.com/shidenggui/easyutils',
-        keywords='China stock trade',
-        install_requires=['requests', 'pyquery'],
-        classifiers=['Development Status :: 4 - Beta',
-                     'Programming Language :: Python :: 2.7',
-                     'Programming Language :: Python :: 3.4',
-                     'Programming Language :: Python :: 3.5',
-                     'License :: OSI Approved :: BSD License'],
+    name='easyutils',
+    version='0.1.6',
+    packages=find_packages(),
+    description='A utility for China Stock',
+    author='shidenggui',
+    author_email='longlyshidenggui@gmail.com',
+    license='BSD',
+    url='https://github.com/shidenggui/easyutils',
+    keywords='China stock trade',
+    install_requires=['requests', 'pyquery'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: BSD License'
+    ],
 )
