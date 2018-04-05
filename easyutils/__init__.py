797423805249d6eb2c860daccf51b8dd9387a1d7
shidenggui <903618848@qq.com>
2018-04-05 16:46:32 +0800

    Bump version: 0.1.6 → 0.1.7

--- easyutils/__init__.py
+++ easyutils/__init__.py
@@ -1,4 +1,4 @@
 # coding:utf8
 from .timeutils import *
 from .stock import *
-__version__ = '0.1.6'
+__version__ = '0.1.7'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easyutils',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     description='A utility for China Stock',
     author='shidenggui',
